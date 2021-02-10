a9f2c7728578d5b8bc839434025a261af411f8f3
doma_cha <62501120+money17@users.noreply.github.com>
2021-02-10 17:26:15 +0900

    transparent home
    + add home container
    + make home container to be opacity zero

--- main.js
+++ main.js
@@ -36,3 +36,11 @@ homeContact.addEventListener('click', () => {
         scrollTo.scrollIntoView({ behavior: 'smooth' });
     };
 });
+
+// Transparent home
+const home = document.querySelector('.home__container')
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', ()=>{
+    home.style.opacity = 1 - window.scrollY / homeHeight
+});
+
