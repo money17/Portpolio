ff7c07adb5e9b1865cf7ae933e139fd177c90814
doma_cha <62501120+money17@users.noreply.github.com>
2021-02-12 19:45:32 +0900

    project animation

--- main.js
+++ main.js
@@ -44,3 +44,46 @@ document.addEventListener('scroll', ()=>{
     home.style.opacity = 1 - window.scrollY / homeHeight
 });
 
+// Show Arrow up button when scrolling down 
+const arrowUp = document.querySelector('.arrow-up')
+document.addEventListener('scroll', ()=> {
+    if(window.scrollY > homeHeight / 2 ){
+        arrowUp.classList.add('visible');
+    } else {
+        arrowUp.classList.remove('visible');
+    }
+});
+
+// Go UP 
+arrowUp.addEventListener('click', ()=>{
+    scrollIntoView('#home');
+    
+});
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior : 'smooth'});
+}
+
+// Project 
+
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e)=> {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if (filter == null){
+        return;
+    }
+    projectContainer.classList.add('anim-out');
+    setTimeout(() => {
+        projects.forEach((project) => {
+            if (filter === "*" ||  filter === project.dataset.type) {
+                project.classList.remove('invisible');
+            } else {
+                project.classList.add('invisible');
+            }
+        })
+        projectContainer.classList.remove('anim-out');
+    }, 300);
+});
